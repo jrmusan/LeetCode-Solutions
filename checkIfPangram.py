d1bb75724d380e2549c3aa5dda3326d2155d5b4f
Joey Musante <jrmusan@gmail.com>
2022-05-31 18:58:29 -0400

    Added simple solution to checkIfPangram

---
+++ checkIfPangram.py
@@ -0,0 +1,6 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        
+        # All we need to do is ensure there are 27 unique letters
+        # If we cast this to a set we will have only unique elements
+        return len(set(sentence)) >=26
